c8ca9ef83ed32d1febef1f52f4553d6e06e58f4c
Carlos Fernando Perez Carranza <22030564@itesa.edu.mx>
2025-08-25 09:38:20 -0600

    Version_02: Formulario DatosAleatorios

---
+++ Datos.Aleatorios/frmDatosAleatorios.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Datos.Aleatorios
+{
+    public partial class frmDatosAleatorios : Form
+    {
+        public frmDatosAleatorios()
+        {
+            InitializeComponent();
+        }
+
+        private void frmDatosAleatorios_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
